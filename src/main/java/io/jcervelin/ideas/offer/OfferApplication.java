d73c460e88a4ec006b48e316e9569b1046c60551
julianocervelintantalum <juliano.cervelin@tantalumcorporation.com>
2018-12-27 10:39:17 +0000

    first commit

---
+++ src/main/java/io/jcervelin/ideas/offer/OfferApplication.java
@@ -0,0 +1,14 @@
+package io.jcervelin.ideas.offer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfferApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfferApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/io/jcervelin/ideas/offer/OfferApplicationTests.java
@@ -0,0 +1,17 @@
+package io.jcervelin.ideas.offer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OfferApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
